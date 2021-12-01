e59ea7216f089f7b18f46c6b3b87d000687c6739
DefaultV <dozermailjd@gmail.com>
2021-12-01 09:22:04 +0100

    Fix pathing issue, load more textures

--- src/planeShader.tsx
+++ src/planeShader.tsx
@@ -7,7 +7,8 @@ const PlaneShader: FunctionComponent = () => {
     const textureArrayRef = useRef<[string, Texture]>();
     const textureRef = useRef<Texture>();
     const textureUrl = "https://upload.wikimedia.org/wikipedia/commons/8/87/Alaskan_Malamute%2BBlank.png";
-    const textureUrl2 = "https://upload.wikimedia.org/wikipedia/commons/8/87/Alaskan_Malamute%2BBlank.png";
+    const textureUrl2 = "http://i.imgur.com/wGyk6os.png";
+    const textureUrl3 = "https://en.wikipedia.org/wiki/Standard_test_image#/media/File:SIPI_Jelly_Beans_4.1.07.tiff"
     textureRef.current = new Texture(textureUrl, null);
     console.log("loaded plane shader component")
   if (textureRef.current) {
@@ -59,6 +60,16 @@ const PlaneShader: FunctionComponent = () => {
               <standardMaterial
               name="mat">
                 <texture url={textureUrl2} name="normal texture" hasAlpha={true}></texture>
+              </standardMaterial>
+            </plane>
+            <plane
+            name="shaderPlane"
+            position-y={1}
+            position-x={0}
+            >
+              <standardMaterial
+              name="mat">
+                <texture url={textureUrl3} name="normal texture" hasAlpha={true}></texture>
               </standardMaterial>
             </plane>
         </transformNode>
