c534b600c35a6b1e3f57e531a148a34901476dbe
DefaultV <dozermailjd@gmail.com>
2022-10-21 11:26:29 +0200

    Eslint unknown property issue

--- src/font-face.d.ts
+++ src/font-face.d.ts
@@ -1,20 +1,24 @@
 // File: src/types/font-face.d.ts
 // Spec: https://www.w3.org/TR/css-font-loading/
-export {}
+export {};
 
 declare global {
   const FontFace: FontFace;
 
   interface Document {
-    fonts: FontFaceSet
+    fonts: FontFaceSet;
   }
 
   type CSSOMString = string;
-  type FontFaceLoadStatus = 'unloaded' | 'loading' | 'loaded' | 'error';
-  type FontFaceSetStatus = 'loading' | 'loaded';
+  type FontFaceLoadStatus = "unloaded" | "loading" | "loaded" | "error";
+  type FontFaceSetStatus = "loading" | "loaded";
 
   interface FontFace extends FontFaceDescriptors {
-    new(family: string, source: string | ArrayBuffer, descriptors?: FontFaceDescriptors): FontFace;
+    new (
+      family: string,
+      source: string | ArrayBuffer,
+      descriptors?: FontFaceDescriptors
+    ): FontFace;
     readonly status: FontFaceLoadStatus;
     readonly loaded: Promise<FontFace>;
     variationSettings: CSSOMString;
@@ -36,8 +40,8 @@ declare global {
     readonly status: FontFaceSetStatus;
     readonly ready: Promise<FontFaceSet>;
     add(font: FontFace): void;
-    check(font: string, text?: string): Boolean; // throws exception
-    load(font: string, text?: string): Promise<FontFace[]>
+    check(font: string, text?: string): boolean; // throws exception
+    load(font: string, text?: string): Promise<FontFace[]>;
     delete(font: FontFace): void;
     clear(): void;
   }
